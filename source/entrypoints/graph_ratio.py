08d27751f48d1c1c557340620bee85809b009ea5
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2023-08-20 10:03:24 -0700

    Update graph_ratio.py

--- source/entrypoints/graph_ratio.py
+++ source/entrypoints/graph_ratio.py
@@ -11,23 +11,38 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
+    dcc.Dropdown(
+        id='graph-dropdown',
+        options=[
+            {'label': 'Iris Scatter Plot', 'value': 'iris_scatter'},
+            {'label': 'Tips Histogram', 'value': 'tips_histogram'}
+        ],
+        value='iris_scatter',
+        clearable=False
+    ),
     dcc.Graph(
         id='example-graph',
         config={'staticPlot': False, 'displayModeBar': True},
-        style={'width': '100%', 'height': '62.5vw'},  # 100% / 1.6 = 62.5%
+        style={'width': '100%', 'height': '62.5vw'}  # 100% / 1.6 = 62.5%
     )
 ])
 
 @app.callback(
     Output('example-graph', 'figure'),
-    Input('example-graph', 'relayoutData'),
+    Input('graph-dropdown', 'value')
 )
-def update_graph(relayoutData):
-    # A simple scatter plot using plotly_express
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", size="petal_length", title="Resize and Watch the Ratio!")
+def update_graph(selected_graph):
+    if selected_graph == 'iris_scatter':
+        df = px.data.iris()
+        fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", size="petal_length")
+    elif selected_graph == 'tips_histogram':
+        df = px.data.tips()
+        fig = px.histogram(df, x="total_bill", color="sex")
+    else:
+        fig = px.scatter()  # default to an empty scatter plot
+
     # Adjust margins
-    fig.update_layout(margin=dict(l=0, r=0, t=30, b=0))
+    fig.update_layout(margin=dict(l=0, r=0, t=40, b=0))
 
     return fig
 
