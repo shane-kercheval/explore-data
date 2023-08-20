a29f42179cf23bf5a6bea33f07fbaccfecd4f199
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2023-08-20 11:52:56 -0700

    Update app.py

--- source/entrypoints/app.py
+++ source/entrypoints/app.py
@@ -73,42 +73,41 @@ app.layout = dbc.Container([
         dbc.Tab(label="Visualize", children=[
             dbc.Row([
                 dbc.Col(width=3, children=[
-                    html.Div(className="split-view", children=[
-                        html.Div(className="options-panel", children=[
-                            html.Label("Select x variable:"),
-                            dcc.Dropdown(
-                                id='x_column_dropdown',
-                                multi=False,
-                                value=None,
-                                placeholder="Select a variable",
-                            ),
-                            html.Label("Select y variable:"),
-                            dcc.Dropdown(
-                                id='y_column_dropdown',
-                                multi=False,
-                                value=None,
-                                placeholder="Select a variable",
-                            ),
-                            html.Label("Graph options:"),
-                            dcc.Slider(
-                                10, 100, 20,
-                                value=40,
-                                id='n_bins',
-                            ),
-                        ]),
+                    html.Br(),
+                    html.Div(className="options-panel", children=[
+                        html.Label("Select x variable:"),
+                        dcc.Dropdown(
+                            id='x_column_dropdown',
+                            multi=False,
+                            value=None,
+                            placeholder="Select a variable",
+                        ),
+                        html.Br(),
+                        html.Label("Select y variable:"),
+                        dcc.Dropdown(
+                            id='y_column_dropdown',
+                            multi=False,
+                            value=None,
+                            placeholder="Select a variable",
+                        ),
+                        html.Br(),
+                        html.Label("Graph options:"),
+                        dcc.Slider(
+                            10, 100, 20,
+                            value=40,
+                            id='n_bins',
+                        ),
                     ]),
                 ]),
                 dbc.Col(width=9, children=[
-                    html.Div(className="visualization-panel", children=[
-                        dcc.Store(id='data_store'),
-                        # dcc.Graph(id='primary-graph'),
-                        dcc.Graph(
-                            id='primary-graph',
-                            config={'staticPlot': False, 'displayModeBar': True},
-                            style={'width': '100%', 'height': '41.9vw'},  # 100% / 1.6 = 62.5%; (1-.33)/1.6 = .41875
-                        ),
-                        dash_table.DataTable(id='table', page_size=20),
-                    ]),
+                    dcc.Store(id='data_store'),
+                    # dcc.Graph(id='primary-graph'),
+                    dcc.Graph(
+                        id='primary-graph',
+                        config={'staticPlot': False, 'displayModeBar': True},
+                        style={'width': '100%', 'height': '41.9vw'},  # 100% / 1.6 = 62.5%; (1-.33)/1.6 = .41875
+                    ),
+                    dash_table.DataTable(id='table', page_size=20),
                 ]),
             ]),
         ]),
