edc13d3d315eb928fbf3d168f5dde30889470f4f
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2023-08-19 19:42:19 -0700

    added tabs

--- source/entrypoints/app.py
+++ source/entrypoints/app.py
@@ -3,70 +3,76 @@ from dash import Dash, html, dash_table, dcc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import numpy as np
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
-    html.H1("CSV Data Visualization"),
-    dcc.Input(
-        id='url-input',
-        type='text',
-        placeholder='Enter CSV URL',
-        value='https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv',
-    ),
-    html.Button('Load', id='load-button', n_clicks=0),
-    html.Br(),html.Br(),
-    html.Label("Select numeric columns:"),
-    dcc.Dropdown(id='column-dropdown', multi=True),
-    dcc.Store(id='data-store'),  # Store component to hold the loaded data
-    dcc.Graph(id='primary-graph'),
-    dash_table.DataTable(id='table', page_size=20),
+    dbc.Tabs([
+        dbc.Tab(label="Load Data", children=[
+            dcc.Input(
+                id='url-input',
+                type='text',
+                placeholder='Enter CSV URL',
+                value='https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv',
+            ),
+            html.Button('Load', id='load-button', n_clicks=0),
+            html.Br(),html.Br(),
+            dash_table.DataTable(id='table3', page_size=20),
+        ]),
+        dbc.Tab(label="Visualize", children=[
+            html.Label("Select numeric columns:"),
+            dcc.Dropdown(id='column-dropdown', multi=True),
+            dcc.Store(id='data-store'),
+            dcc.Graph(id='primary-graph'),
+            dash_table.DataTable(id='table', page_size=20),
+        ]),
+    ]),
 ])
+
 
 @app.callback(
     [
         Output('column-dropdown', 'options'),
         Output('column-dropdown', 'value'),
-        Output('data-store', 'data'),  # Update the data in the store
+        Output('data-store', 'data'),
         Output('table', 'data'),
+        Output('table3', 'data'),
     ],
     [
-        Input('load-button', 'n_clicks'),
+        Input('load-button', 'n_clicks')
     ],
     [
-        State('url-input', 'value'),
+        State('url-input', 'value')
     ],
 )
 def load_data(n_clicks: int, url: str) -> tuple:
     """Triggered when the user clicks on the Load button."""
     if n_clicks > 0 and url:
-        loaded_data = pd.read_csv(url)
-        numeric_columns = loaded_data.select_dtypes(include=[np.number]).columns
-        dropdown_options = [{'label': col, 'value': col} for col in numeric_columns]
-        loaded_data = loaded_data.to_dict('records')
-        return dropdown_options, [], loaded_data, loaded_data  # Store data in the store
-    return [], [], None, None
+        data = pd.read_csv(url)
+        numeric_cols = data.select_dtypes(include=[np.number]).columns
+        options = [{'label': col, 'value': col} for col in numeric_cols]
+        data = data.to_dict('records')
+        return options, [], data, data, data
+    return [], [], None, None, None
+
 
 @app.callback(
-    [
-        Output('primary-graph', 'figure'),
-    ],
-    [
-        Input('column-dropdown', 'value'),
-    ],
-    [
-        State('data-store', 'data'),
-    ],  # Access the data from the store
+    Output('primary-graph', 'figure'),
+    [Input('column-dropdown', 'value')],
+    [State('data-store', 'data')]
 )
-def update_histogram(selected_columns: str, data_store: dict) -> dict:
-    """Triggered when the user selects a column from the dropdown."""
-    if data_store and selected_columns:
-        loaded_data = pd.DataFrame.from_records(data_store)  # Convert stored data to DataFrame
-        data = []
+def update_graph(selected_columns: str, data: dict) -> dict:
+    """Triggered when the user selects columns from the dropdown."""
+    if selected_columns and data:
+        data = pd.DataFrame(data)
+        graphs = []
         for col in selected_columns:
-            data.append({'x': loaded_data[col], 'type': 'histogram', 'name': col})
-        return {'data': data, 'layout': {'title': 'Histogram'}}
-    return {'data': [], 'layout': {'title': 'Histogram'}}
+            graphs.append({'x': data[col], 'type': 'histogram', 'name': col})
+        return {'data': graphs, 'layout': {'title': 'Histogram'}}
+    return {'data': [], 'layout': {}}
+
 
 
 if __name__ == '__main__':
