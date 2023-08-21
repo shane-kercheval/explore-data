977c0e2bdc1d9e303736455d0e63222f9e81d5ef
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2023-08-20 21:20:28 -0700

    updated sandbox files

--- source/entrypoints/dynamic_graph_options.py
+++ source/entrypoints/dynamic_graph_options.py
@@ -1,114 +1,43 @@
-import base64
-import io
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output, State
-import pandas as pd
-import plotly.express as px
-import os
+from dash import Dash, dcc, html, Input, Output, ALL, Patch, callback
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div([
-    html.Div([
-        dcc.Input(id='url-input', placeholder='Enter a URL...', type='text'),
-        dcc.Upload(
-            id='upload-data',
-            children=html.Div(['Drag and Drop or ', html.A('Select Files')]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px',
-            },
-            multiple=False,
-        ),
-        html.Button('Load Data', id='load-button'),
-    ]),
-    html.Div(id='storage', style={'display': 'none'}),
-    html.Div(id='column-select-div'),
-    html.Div(id='graph-options-div'),
-    html.Div(id='display-graph')
-])
-
-@app.callback(
-    Output('storage', 'children'),
-    Input('load-button', 'n_clicks'),
-    State('url-input', 'value'),
-    State('upload-data', 'contents'),
-    prevent_initial_call=True
+app.layout = html.Div(
+    [
+        html.Button("Add Filter", id="add-filter-btn", n_clicks=0),
+        html.Div(id="dropdown-container-div", children=[]),
+        html.Div(id="dropdown-container-output-div"),
+    ]
 )
-def load_data(n, url, contents):
-    if url:
-        df = pd.read_csv(url)
-    elif contents:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        try:
-            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-        except:
-            df = pd.read_pickle(io.BytesIO(decoded))
-    df.to_pickle('temp.pkl')
-    return 'Data Loaded'
 
-@app.callback(
-    Output('column-select-div', 'children'),
-    Input('storage', 'children'),
-    prevent_initial_call=True
+
+@callback(
+    Output("dropdown-container-div", "children"),
+    Input("add-filter-btn", "n_clicks"),
 )
-def select_columns(storage_content):
-    if storage_content == 'Data Loaded':
-        df = pd.read_pickle('temp.pkl')
-        options = [{'label': col, 'value': col} for col in df.columns]
-        return [
-            dcc.Checklist(options=options, id='column-checklist'),
-            html.Button('Select Columns', id='select-columns-button')
-        ]
-    return []
+def display_dropdowns(n_clicks):
+    patched_children = Patch()
+    new_dropdown = dcc.Dropdown(
+        ["NYC", "MTL", "LA", "TOKYO"],
+        multi=True,
+        id={"type": "city-filter-dropdown", "index": str(n_clicks) + 's'},
+    )
+    patched_children.append(new_dropdown)
+    return patched_children
 
-@app.callback(
-    Output('graph-options-div', 'children'),
-    Input('select-columns-button', 'n_clicks'),
-    State('column-checklist', 'value'),
-    prevent_initial_call=True
+
+@callback(
+    Output("dropdown-container-output-div", "children"),
+    Input({"type": "city-filter-dropdown", "index": ALL}, "value"),
+    Input({"type": "city-filter-dropdown", "index": ALL}, "id"),
 )
-def graph_options(n, columns):
-    if not columns:
-        return []
-    df = pd.read_pickle('temp.pkl')
-    column_types = df[columns].dtypes
-    if 'object' in column_types.values:
-        return [dcc.Dropdown(options=[{'label': 'Bar Chart', 'value': 'bar'}, {'label': 'Pie Chart', 'value': 'pie'}], id='graph-dropdown')]
-    else:
-        return [dcc.Dropdown(options=[{'label': 'Line Chart', 'value': 'line'}, {'label': 'Scatter Plot', 'value': 'scatter'}], id='graph-dropdown')]
+def display_output(values, indexes):
+    print(f"values: {values}", flush=True)
+    print(f"indexes: {indexes}", flush=True)
+    return html.Div(
+        [html.Div(f"Dropdown {i + 1} = {value}") for (i, value) in enumerate(values)]
+    )
 
-@app.callback(
-    Output('display-graph', 'children'),
-    Input('graph-dropdown', 'value'),
-    State('column-checklist', 'value'),
-    prevent_initial_call=True
-)
-def display_graph(graph_type, columns):
-    df = pd.read_pickle('temp.pkl')
-    if not columns:
-        return []
-    if graph_type == 'bar':
-        fig = px.bar(df, x=columns[0], y=columns[1:])
-    elif graph_type == 'pie':
-        fig = px.pie(df, names=columns[0], values=columns[1])
-    elif graph_type == 'line':
-        fig = px.line(df, x=columns[0], y=columns[1:])
-    elif graph_type == 'scatter':
-        fig = px.scatter(df, x=columns[0], y=columns[1])
-    else:
-        return []
-    return dcc.Graph(figure=fig)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(host='0.0.0.0', debug=True, port=8050)
